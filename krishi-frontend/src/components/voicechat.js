2117d2ddf6d59711736fbab9b68c6be531757d72
VN1702 <varsha@gmail.com>
2026-03-28 10:27:46 +0530

    last changes

--- krishi-frontend/src/components/voicechat.js
+++ krishi-frontend/src/components/voicechat.js
@@ -62,7 +62,7 @@ function VoiceChat() {
     setChatHistory((prev) => [...prev, userMsg]);
 
     try {
-      const res = await fetch("http://localhost:5000/api/chat", {
+      const res = await fetch("http://localhost:5000/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
